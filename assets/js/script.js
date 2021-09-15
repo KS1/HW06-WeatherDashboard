28c91f1413d1bf795082922446eedc8a7026001d
Kartiki Saraf <kartikisaraf@yahoo.com>
2021-09-14 19:55:52 -0700

    Added refined code with comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,12 @@
-
-// https://openweathermap.org/api
-// https://home.openweathermap.org/ 
-// https://openweathermap.org/api/one-call-api 
-
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} 
-
-// https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=400a5cae84cd4ce01fdd89b862f1e430
-
-// // Used constant to store API Key
-// const apiKey = "400a5cae84cd4ce01fdd89b862f1e430";
-// var lat = "33.44"; // latitude
-// var lon = "-94.04"; // longitude
-// var part = "hourly,daily"; // exclude
-// // Used template literal for URL
-// var openAPIUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`;
-// console.log("openAPIUrl: " + openAPIUrl);
-
-// //////////////////////////////////////////////////////////////////////////
+// script for weather dashboard application
 
 var cityInput = document.querySelector('#city-input');
 var cityBtn = document.querySelector('#search-btn');
 var cityNameEl = document.querySelector('#city-name');
 var cityArr = [];
-// var apiKey = 'e4ab7318fab329c7de8c4fd9dd5056d7'; 
-var apiKey = '400a5cae84cd4ce01fdd89b862f1e430'; //kartiki
+
+// set api key
+var apiKey = '400a5cae84cd4ce01fdd89b862f1e430'; 
 
 var formHandler = function(event) {
     // formats city name
@@ -43,15 +26,18 @@ var formHandler = function(event) {
     };
 };
 
-// uses 'current weather api' to fetch latitude and longitude
+// use 'current weather api' to fetch latitude and longitude
 var getCoords = function(city) {
+    // Used template literal for URL
     var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
     fetch(currentWeatherApi).then(function(response) {
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function(data) { 
                 var lon = data.coord['lon'];
                 var lat = data.coord['lat'];
+
+                // get city forecast
                 getCityForecast(city, lon, lat);
 
                 // saves searched city and refreshes recent city list
@@ -59,7 +45,9 @@ var getCoords = function(city) {
                     document.querySelector('.city-list').remove();
                 }
 
+                // save city
                 saveCity(city);
+                // load cities
                 loadCities();
             });
         } else {
@@ -71,17 +59,15 @@ var getCoords = function(city) {
     })
 }
 
-// uses latitude and longitude to fetch current weather and five-day forecast
+// use latitude and longitude to fetch current weather and five-day forecast
 var getCityForecast = function(city, lon, lat) {
     var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
     fetch(oneCallApi).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
 
-                // identifies city name in forecast
+                // identify city name in forecast
                 cityNameEl.textContent = `${city} (${moment().format("M/D/YYYY")})`; 
-
-                console.log(data)
 
                 currentForecast(data);
                 fiveDayForecast(data);
@@ -97,39 +83,45 @@ var displayTemp = function(element, temperature) {
     tempEl.textContent = elementText;
 }
 
-// displays current forecast
+// display current forecast
 var currentForecast = function(forecast) {
     
     var forecastEl = document.querySelector('.city-forecast');
     forecastEl.classList.remove('hide');
 
+    // set today's weather icon
     var weatherIconEl = document.querySelector('#today-icon');
     var currentIcon = forecast.current.weather[0].icon;
     weatherIconEl.setAttribute('src', `http://openweathermap.org/img/wn/${currentIcon}.png`);
     weatherIconEl.setAttribute('alt', forecast.current.weather[0].main)
 
+    // set current temparature
     displayTemp('#current-temp', forecast.current['temp']);
     displayTemp('#current-feels-like', forecast.current['feels_like']);
     displayTemp('#current-high', forecast.daily[0].temp.max);
     displayTemp('#current-low', forecast.daily[0].temp.min);
 
+    // set current weather condition
     var currentConditionEl = document.querySelector('#current-condition');
     currentConditionEl.textContent = forecast.current.weather[0].description
         .split(' ')
         .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
         .join(' ');
 
+    // set humidity
     var currentHumidityEl = document.querySelector('#current-humidity');
     currentHumidityEl.textContent = forecast.current['humidity'];
 
+    // set wind-speed
     var currentWindEl = document.querySelector('#current-wind-speed')
     currentWindEl.textContent = forecast.current['wind_speed'];
 
+    // set uv index
     var uviEl = document.querySelector('#current-uvi')
     var currentUvi = forecast.current['uvi'];
     uviEl.textContent = currentUvi;
 
-    // styles UV index
+    // style UV index
     switch (true) {
         case (currentUvi <= 2):
             uviEl.className = 'badge badge-success';
@@ -167,10 +159,10 @@ var fiveDayForecast = function(forecast) {
     }
 }
 
-// saves cities into local storage
+// save cities into local storage
 var saveCity = function(city) {
 
-    // prevents duplicate city from being saved and moves it to end of array
+    // prevent duplicate city from being saved and move it to end of array
     for (var i = 0; i < cityArr.length; i++) {
         if (city === cityArr[i]) {
             cityArr.splice(i, 1);
@@ -181,7 +173,7 @@ var saveCity = function(city) {
     localStorage.setItem('cities', JSON.stringify(cityArr));
 }
 
-// loads cities from local storage
+// load cities from local storage
 var loadCities = function() {
     cityArr = JSON.parse(localStorage.getItem('cities'));
 
@@ -189,10 +181,11 @@ var loadCities = function() {
         cityArr = [];
         return false;
     } else if (cityArr.length > 5) {
-        // saves only the five most recent cities
+        // save only the five most recent cities
         cityArr.shift();
     }
 
+    // set recent cities
     var recentCities = document.querySelector('#recent-cities');
     var cityListUl = document.createElement('ul');
     cityListUl.className = 'list-group list-group-flush city-list';
@@ -214,51 +207,20 @@ var loadCities = function() {
 var selectRecent = function(event) {
     var clickedCity = event.target.getAttribute('value');
 
+    // get co-ordinates
     getCoords(clickedCity);
 }
 
+// call load cities function on page load
 loadCities();
+
+// set event listener for city button
 cityBtn.addEventListener('click', formHandler)
 
-// searches for city on ENTER key
+// searche for city on ENTER key
 cityInput.addEventListener('keyup', function(event) {
     if (event.keyCode === 13) {
         cityBtn.click();
     }
 });
 
-
-
-// //////////////////////////////////////////////////////////////////////////
-// function getApi() {
-//     // fetch request gets a list of all the repos for the node.js organization
-//     var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
-
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         console.log(data)
-//         //Loop over the data to generate a table, each table row will have a link to the repo url
-//         for (var i = 0; i < data.length; i++) {
-//           // Creating elements, tablerow, tabledata, and anchor
-//           var createTableRow = document.createElement('tr');
-//           var tableData = document.createElement('td');
-//           var link = document.createElement('a');
-  
-//           // Setting the text of link and the href of the link
-//           link.textContent = data[i].html_url;
-//           link.href = data[i].html_url;
-  
-//           // Appending the link to the tabledata and then appending the tabledata to the tablerow
-//           // The tablerow then gets appended to the tablebody
-//           tableData.appendChild(link);
-//           createTableRow.appendChild(tableData);
-//           tableBody.appendChild(createTableRow);
-//         }
-//       });
-//   }
-  
-//   fetchButton.addEventListener('click', getApi);
-  
